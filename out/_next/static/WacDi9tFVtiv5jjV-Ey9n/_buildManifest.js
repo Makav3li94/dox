8314823e8cc3dc44bf095ab66cba9eee10cf20f4
Parham <37364144+Makav3li94@users.noreply.github.com>
2023-03-12 19:57:27 +0330

    first

---
+++ out/_next/static/WacDi9tFVtiv5jjV-Ey9n/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-18309691bcd4e51f.js"],"/_error":["static/chunks/pages/_error-a4ba2246ff8fb532.js"],"/about":["static/chunks/pages/about-597e0078446e1343.js"],sortedPages:["/","/_app","/_error","/about"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
